eadf779c2d7395eeb1dd0d2d5c20c3f41df0198c
Gongda Ge <gegongda92@gmail.com>
2020-07-14 22:06:40 -0400

    implement format

--- src/format.cpp
+++ src/format.cpp
@@ -4,8 +4,11 @@
 
 using std::string;
 
-// TODO: Complete this helper function
-// INPUT: Long int measuring seconds
-// OUTPUT: HH:MM:SS
-// REMOVE: [[maybe_unused]] once you define the function
-string Format::ElapsedTime(long seconds[[maybe_unused]]) { return string(); }
+string Format::ElapsedTime(long seconds) {
+    int h, m, s;
+    h = seconds / 3600;
+    m = (seconds - (3600 * h)) / 60;
+    s = (seconds -(3600 * h) - (m * 60));
+    return std::to_string(h) + ":" + std::to_string(m) + ":" +
+           std::to_string(s);
+}
